90e9c197368fbe18cb5995cdda147a167212114b
Vilius Šapauskas <v.sapauskas.1997@gmail.com>
2020-11-18 00:12:03 +0200

    Remove unused files from Web project, modify .gitignore, and add .vscode folder

---
+++ src/DCT.Web/Controllers/TaxesController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DCT.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxesController : ControllerBase
+    {
+        private readonly ILogger<TaxesController> _logger;
+
+        public TaxesController(ILogger<TaxesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            return new List<int>();
+        }
+    }
+}
