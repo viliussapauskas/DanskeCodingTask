ae239ba3ea7b96329cc2587622b6f31afdb88b75
Vilius Šapauskas <v.sapauskas.1997@gmail.com>
2020-11-20 00:59:14 +0200

    Add axios, created initial request to get municipalities, remove unused code inf front-end, fix errors

--- src/DCT.Web/client-app/src/App.tsx
+++ src/DCT.Web/client-app/src/App.tsx
@@ -1,8 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  const [municipalities, setMunicipalities] = useState<any[] | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("http://localhost:5000/api/municipality");
+      setMunicipalities(result.data);
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <div className="App">
+      {!municipalities && <div>Loading...</div>}
+      {municipalities && (
+        <div>
+          {municipalities.map((x, i) => (
+            <div key={i}>{x.name}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
