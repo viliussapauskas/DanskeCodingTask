47ea9c3522dd3e16ae69017454eddbb1faea2c81
Vilius Šapauskas <v.sapauskas.1997@gmail.com>
2020-11-21 17:35:16 +0200

    Add logging to services, remove unused code

--- src/DCT.Application/Services/MunicipalityService.cs
+++ src/DCT.Application/Services/MunicipalityService.cs
@@ -1,24 +1,32 @@
 ﻿using DCT.Application.Models;
 using DCT.Application.Services.Interfaces;
 using DCT.Persistence.Repositories.Interfaces;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DCT.Application.Services
 {
-    public class MunicipalityService: IMunicipalityService
+    public class MunicipalityService : IMunicipalityService
     {
         private readonly IMunicipalityRepository _municipalityRepository;
+        private readonly ILogger<MunicipalityService> _logger;
 
-        public MunicipalityService(IMunicipalityRepository municipalityRepository)
+        public MunicipalityService(IMunicipalityRepository municipalityRepository, ILogger<MunicipalityService> logger)
         {
             _municipalityRepository = municipalityRepository;
+            _logger = logger;
         }
 
         public async Task<List<MunicipalityDTO>> GetAll()
         {
             var entities = await _municipalityRepository.GetAll();
+
+            if (!entities.Any())
+            {
+                _logger.LogWarning("_municipalityRepository.GetAll() returns empty list");
+            }
 
             return entities.Select(x => new MunicipalityDTO
             {
--- src/DCT.Application/Services/TaxesService.cs
+++ src/DCT.Application/Services/TaxesService.cs
@@ -1,27 +1,31 @@
 ﻿using DCT.Application.Services.Interfaces;
 using DCT.Persistence.Repositories.Interfaces;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
 namespace DCT.Application.Services
 {
-    public class TaxesService: ITaxesService
+    public class TaxesService : ITaxesService
     {
         private readonly IMunicipalityRepository _municipalityRepository;
         private readonly ITaxesCalculationService _taxesCalculationService;
+        private readonly ILogger<ITaxesService> _logger;
 
-        public TaxesService(IMunicipalityRepository municipalityRepository, ITaxesCalculationService taxesCalculationService)
+        public TaxesService(IMunicipalityRepository municipalityRepository, ITaxesCalculationService taxesCalculationService, ILogger<ITaxesService> logger)
         {
             _municipalityRepository = municipalityRepository;
             _taxesCalculationService = taxesCalculationService;
+            _logger = logger;
         }
 
         public async Task<double> GetTaxes(int municipalityId, DateTime date)
         {
             var municipality = await _municipalityRepository.GetById(municipalityId);
 
-            if(municipality == null)
+            if (municipality == null)
             {
+                _logger.LogError($"Municipality with id {municipalityId} not found");
                 throw new Exception("Municipality not found");
             }
 
