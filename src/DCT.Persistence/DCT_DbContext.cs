42fe849e00d04dd7148a8a18b796e0165ac0657d
Vilius Šapauskas <v.sapauskas.1997@gmail.com>
2020-11-22 20:26:04 +0200

    Format and remove unused code, add material ui package

--- src/DCT.Persistence/DCT_DbContext.cs
+++ src/DCT.Persistence/DCT_DbContext.cs
@@ -1,23 +1,15 @@
 ﻿using DCT.Persistence.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace DCT.Persistence
 {
     public partial class DCT_DbContext : DbContext
     {
-        private readonly IConfiguration configuration;
-
-        public DCT_DbContext(DbContextOptions<DCT_DbContext> options, IConfiguration configuration)
+        public DCT_DbContext(DbContextOptions<DCT_DbContext> options)
             : base(options)
         {
-            this.configuration = configuration;
         }
 
         public DbSet<Municipality> Municipalities { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-        }
     }
 }
--- src/DCT.Web/client-app/src/utils/apiFunctionts.ts
+++ src/DCT.Web/client-app/src/utils/apiFunctionts.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import { API_URL } from './constants';
 
-export const getMunicipalities = () =>
-  axios.get(`${API_URL}/municipality`);
+export const getMunicipalities = () => axios.get(`${API_URL}/municipality`);
 
 export const getTaxes = (municipalityId: number, date: string) =>
 axios.get(`${API_URL}/taxes/municipalities/${municipalityId}/date/${date}`)
